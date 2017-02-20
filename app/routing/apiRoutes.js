d542b5e10595c95a19fb722515c9fdce5d73c1e8
Ysa Tan <tanysaur@gmail.com>
2017-02-19 21:18:09 -0600

    updated apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,9 +2,58 @@
 //
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
+var path = require('path');
 var friendsList = require("../data/friends.js");
 
+// var userResultArray = [];
+// var totalDifference;
+
+// Compare array of newUser with all users in friendsList array
+// console.log(JSON.stringify(absoluteDifference));
+
+// Take absoluteDifference of the two arrays, let score = sum of absoluteDifference
+
+// highestScore = higest value of score
+
+// The modal should display both the name and picture of the closest match.
+
+
+// Finds the new user's match
+function findNewBff(friendsArray) {
+  var diffArray = [];
+  var matches = [];
+
+  // Loops through friendsList data
+  for(var i = 0; i < friendsList.length; i++) {
+
+    var comparisonArray = friendsList[i].scores;
+
+    // Sum of the absolute difference of the array
+    var sumOfAbsDiff = comparisonArray.reduce(function(acc, elem, index) {
+      return acc + Math.abs(elem - friendsArray[index]);
+    },0);
+    console.log(comparisonArray);
+    //push differences and sum value to diffArray
+    diffArray.push(sumOfAbsDiff);
+  }
+
+  // Then loop through the new diffArray and look for the lowest number to find the match
+  var arrayIndex = 0;
+  var value = diffArray[0];
+  for (var p = 1; p < diffArray.length - 1; p++) {
+    if (diffArray[p] < value) {
+      value = diffArray[p];
+      arrayIndex = p;
+    }
+  }
+
+  // Push the new BFF to the matches array
+  matches.push(friendsList[arrayIndex]);
+
+  console.log(matches);
+
+  return matches;
+  }
 // Routes
 // =============================================================
 module.exports = function(app) {
@@ -14,15 +63,25 @@ module.exports = function(app) {
     res.json(friendsList);
   });
 
-  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-  app.post("/api/friends", function(req, res){
-    friendsList.push(req.body);
-    
+  app.get("/survey", function(req, res){
+    res.send(userData);
   });
 
+  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post("/api/friends", function(req, res){
 
+    var data = req.body;
+    //find friend matches for user
+    var matches = findNewBff(data.scores);
+
+    friendsList.push(data);
+
+    console.log(req.body);
+    console.log(req.body.scores);
+    //add user's info to database (push into array of objects)
+    friendsList.push(data);
+    //send back to front-end the array of matches
+    res.send(matches);
+
+  });
 };
-
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]). With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference. Example: User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1] User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1] Total Difference: 2 + 1 + 2 = 5 Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on. The closest match will be the user with the least amount of difference. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//
-// The modal should display both the name and picture of the closest match.
