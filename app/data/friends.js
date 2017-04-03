ed7e06761b2da3bdfd07c33837128791f6299e56
Ysa Tan <tanysaur@gmail.com>
2017-04-02 21:23:36 -0500

    Final - working

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,57 +4,159 @@ var friendsList = [
   {
     "name": "Amy Pond",
     "photo": "https://upload.wikimedia.org/wikipedia/commons/b/be/Amy_Pond_cropped.jpg",
-    "scores": [5,1,2,4,1,3,4,1,2,3]
+    "scores": [ "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"]
   },
   {
     "name": "Clara Oswald",
     "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Clara_Oswald.jpg/400px-Clara_Oswald.jpg",
-    "scores":[4,1,3,4,2,3,5,1,2,1]
+    "scores":[ "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"]
   },
   {
     "name": "Rose Tyler",
     "photo": "https://upload.wikimedia.org/wikipedia/en/7/74/Rose_Tyler.jpg",
-    "scores":[4,2,1,1,0,3,4,3,4,2]
+    "scores":["5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"]
   },
   {
     "name": "Rory Williams",
     "photo": "https://upload.wikimedia.org/wikipedia/en/4/43/RoryWilliams.jpg",
-    "scores":[1,3,2,1,3,4,3,0,2,2]
+    "scores":["3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
   },
   {
     "name": "Donna Noble",
     "photo": "https://upload.wikimedia.org/wikipedia/en/d/dd/Donna_Noble.jpg",
-    "scores": [4,3,2,2,3,4,3,2,2,2]
+    "scores": [ "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"]
   },
   {
     "name": "Martha Jones",
     "photo": "https://upload.wikimedia.org/wikipedia/en/3/3b/Martha_Jones.jpg",
-    "scores":[3,3,3,4,4,2,4,2,0,2]
+    "scores":[ "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"]
   },
   {
     "name": "River Song",
     "photo": "https://upload.wikimedia.org/wikipedia/en/5/56/Riversong.jpg",
-    "scores":[1,3,5,2,1,5,2,3,2,1]
+    "scores": [ "2",
+      "5",
+      "3",
+      "1",
+      "4",
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",]
   },
   {
     "name": "Sarah Jane Smith",
     "photo": "https://upload.wikimedia.org/wikipedia/en/0/0e/Sarah_Jane_Smith_2006.jpg",
-    "scores": [3,4,5,1,3,2,1,1,4,1]
+    "scores": [   "2",
+      "5",
+      "3",
+      "1",
+      "4",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1"]
   },
   {
     "name": "Mickey Smith",
     "photo": "https://upload.wikimedia.org/wikipedia/en/b/b5/Mickey_Smith.jpg",
-    "scores": [1,1,3,4,4,1,2,4,5,1]
+    "scores": ["3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+]
   },
   {
     "name": "Jack Harkness",
     "photo": "https://upload.wikimedia.org/wikipedia/en/7/7d/Jack_Harkness.jpg",
-    "scores":[2,4,1,5,5,5,1,2,3,1]
+    "scores":["2",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "1",
+      "2",]
   },
   {
     "name": "The Doctor",
     "photo": "https://upload.wikimedia.org/wikipedia/en/8/8a/Eleventhdoctornew.jpg",
-    "scores": [5,5,5,5,5,5,5,5,5,5]
+    "scores": [
+      "5",
+      "4",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1",
+      "5",
+      "4",
+      "1"]
   },
 
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,58 +2,9 @@
 //
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-var path = require('path');
+// var path = require('path');
 var friendsList = require("../data/friends.js");
 
-// var userResultArray = [];
-// var totalDifference;
-
-// Compare array of newUser with all users in friendsList array
-// console.log(JSON.stringify(absoluteDifference));
-
-// Take absoluteDifference of the two arrays, let score = sum of absoluteDifference
-
-// highestScore = higest value of score
-
-// The modal should display both the name and picture of the closest match.
-
-
-// Finds the new user's match
-function findNewBff(friendsArray) {
-  var diffArray = [];
-  var matches = [];
-
-  // Loops through friendsList data
-  for(var i = 0; i < friendsList.length; i++) {
-
-    var comparisonArray = friendsList[i].scores;
-
-    // Sum of the absolute difference of the array
-    var sumOfAbsDiff = comparisonArray.reduce(function(acc, elem, index) {
-      return acc + Math.abs(elem - friendsArray[index]);
-    },0);
-    console.log(comparisonArray);
-    //push differences and sum value to diffArray
-    diffArray.push(sumOfAbsDiff);
-  }
-
-  // Then loop through the new diffArray and look for the lowest number to find the match
-  var arrayIndex = 0;
-  var value = diffArray[0];
-  for (var p = 1; p < diffArray.length - 1; p++) {
-    if (diffArray[p] < value) {
-      value = diffArray[p];
-      arrayIndex = p;
-    }
-  }
-
-  // Push the new BFF to the matches array
-  matches.push(friendsList[arrayIndex]);
-
-  console.log(matches);
-
-  return matches;
-  }
 // Routes
 // =============================================================
 module.exports = function(app) {
@@ -63,25 +14,59 @@ module.exports = function(app) {
     res.json(friendsList);
   });
 
-  app.get("/survey", function(req, res){
-    res.send(userData);
-  });
+  // app.get("/survey", function(req, res){
+  //   res.send(userData);
+  // });
 
   // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post("/api/friends", function(req, res){
 
-    var data = req.body;
-    //find friend matches for user
-    var matches = findNewBff(data.scores);
+    // We will use this object to hold the "best match". We will constantly update it as we
+    // loop through all of the options
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
 
-    friendsList.push(data);
 
-    console.log(req.body);
-    console.log(req.body.scores);
-    //add user's info to database (push into array of objects)
-    friendsList.push(data);
-    //send back to front-end the array of matches
-    res.send(matches);
+// Here we take the result of the user"s survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    // This variable will calculate the difference between the user"s scores and the scores of
+    // each user in the database
+    var totalDifference = 0;
+
+    // Here we loop through all the friend possibilities in the database.
+    for (var i = 0; i < friendsList.length; i++) {
+
+      console.log(friendsList[i].name);
+      totalDifference = 0;
+
+      // We then loop through all the scores of each friend
+      for (var j = 0; j < friendsList[i].scores[j]; j++) {
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsList[i].scores[j]));
+
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = friendsList[i].name;
+          bestMatch.photo = friendsList[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+
+    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+    // the database will always return that the user is the user's best friend).
+    friendsList.push(userData);
+
+    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+    res.json(bestMatch);
 
   });
 };
